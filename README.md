b4c384bdb7fce12394214a1380b89287e9dac92b
kyeongrok <oceanfog1@gmail.com>
2019-09-05 09:55:12 +0900

    main -> cli

--- README.md
+++ README.md
@@ -7,9 +7,9 @@
 이 프로그램은 신용카드 결제 시스템 입니다.
 
 사용 방법:
-    main.py [file_path]
+    cli.py [file_path]
 
 예제:
-    python main.py < ./inputs/input.txt
-    python main.py ./inputs/input.txt
+    python cli.py < ./inputs/input.txt
+    python cli.py ./inputs/input.txt
 ```
---
+++ cli.py
@@ -0,0 +1,22 @@
+from ccpayment.command_parser import Parser
+from ccpayment.command_processor import Processor
+import sys
+
+parser = Parser()
+processor = Processor()
+
+
+def main():
+    commands = []
+    if len(sys.argv) > 1:
+        file = open(sys.argv[1])
+        lines = file.readlines()
+        commands = parser.get_commands(lines)
+    else:
+        commands = parser.get_commands(sys.stdin)
+
+    result = processor.process(commands)
+    processor.print_result(result)
+
+
+main()
